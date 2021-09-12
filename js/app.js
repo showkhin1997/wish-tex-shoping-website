eded3f4d5ad57b5fb394e0606c42bc53e5e04f87
showkhin1997 <showkhin15@gmail.com>
2021-09-13 03:49:04 +0600

    finding total

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
+    // fixed image not show 
     const image = product.image;
     const div = document.createElement("div");
     div.classList.add("product");
@@ -76,4 +77,5 @@ const updateTotal = () => {
     getInputValue("total-tax");
   document.getElementById("total").innerText = grandTotal;
 };
+updateTotal();
 loadProducts();
