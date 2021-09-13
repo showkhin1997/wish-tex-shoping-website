8975e073d833b816d62c32e30aca18d7b3fcf63f
showkhin1997 <showkhin15@gmail.com>
2021-09-13 20:10:18 +0600

    product card update

--- js/app.js
+++ js/app.js
@@ -16,13 +16,13 @@ const showProducts = (products) => {
       <div>
     <img class="product-image" src=${image}></img>
       </div>
-      <h3>${product.title}</h3>
+      <h3 class="product-titile">${product.title}</h3>
       <p>Category: ${product.category}</p>
       <p>Rating: <span class="rating">${product.rating.rate}</span></p>
       <p>Total Rating Count: <span class="rating">${product.rating.count}</span></p>
       <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
+      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart<i class="fas fa-cart-plus"></i></button>
+      <button id="details-btn" class="btn btn-danger">Details<i class="fas fa-info-circle"></i></button></div>
       `;
     document.getElementById("all-products").appendChild(div);
   }
